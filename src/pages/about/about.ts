fcc19b87dedd0cd8144d40658e9d86f5a18117b0
Jaime Pérez Aparicio <19.jaime.91@gmail.com>
2017-05-07 21:04:27 +0200

    El contenido no llega al cambiar el estado del check. Hay que comprobar el valor de "local".

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -9,19 +9,26 @@ import { Response } from '@angular/http';
 })
 export class AboutPage {
   info: Response;
+  infojson: any={
+    "reps": []
+  };
+  content: string='';
   uid: Number=1;
-  pattern: String="Alphabet";
-  busq: String="Google";
-  pattern_: String=".*";
+  pattern: string="Alphabet";
+  busq: string="Google";
+  pattern_: string=".*";
   local: Boolean=false
-  prev: String="Google";
-  prevpat: String=".*";
+  prev: string="Google";
+  prevpat: string=".*";
 
   constructor(public navCtrl: NavController, public infoService: InfoService) {
     this.getInfo();
   }
 
   getInfo(){
+    /*
+      Obtiene la informacion del servidor si es necesario.
+     */
     if (this.local){
       this.pattern_ = ".*";
     } else {
@@ -36,6 +43,10 @@ export class AboutPage {
   }
 
   checkChanged(){
+    /*
+      Se ejecuta cuando se cambia el estado del checkbox.
+      Vuelve a pedir los datos del servidor.
+     */
     if (this.local){
       this.pattern_ = ".*";
     } else {
@@ -46,14 +57,38 @@ export class AboutPage {
   }
 
   filter(){
-
+    /*
+      Si se está trabajando en local, filtra el texto.
+     */
+    if (this.local){
+      this.countWords();
+    }
   }
 
   getInfoCall(){
+    /*
+    Obtiene la informacion del servidor.
+     */
     this.infoService.getDataPromise(this.uid, this.pattern_, this.busq, this.local)
       .subscribe(data => {
-        this.info = data.json();
+        this.info = data;
+        this.infojson = data.json();
       });
   }
 
+  countWords() {
+    /*
+    Cuenta el numero de repeticiones de la palabra dada.
+     */
+    var regexp = new RegExp(this.pattern, "ig");
+    regexp.compile();
+    console.log(this.infojson.reps);
+    for (let element of this.infojson.reps){
+      var content = element.content;
+      var result = regexp.exec(content);
+      element.num = result.length;
+      console.log(result);
+      console.log(element.num);
+    }
+  }
 }
